85fc1dd44ddb99c80346c048c9654405a3c8ebb5
ktdthoainam <ktdthoainam@gmail.com>
2023-12-03 22:23:22 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import React,{useState} from 'react';
+import UseEffect from './component/UseEffect';
 
 function App() {
+  const [toggle, setToggle]= useState(false)
+
   return (
     <div className="container">
-      
+    <button onClick={()=>setToggle(!toggle)}>toogle Componennt</button>
+    {toggle && <UseEffect/>}
     </div>
   );
 }
---
+++ src/component/UseEffect.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+ function UseEffect(){
+    return(
+        <div>
+            <h1>UseEffect</h1>
+        </div>
+    )
+ }
+ export default UseEffect;
